d37e7d1e6d09983121ae75d51f49cce0d07559a2
Katya <lopukhina.ekaterina@gmail.com>
2019-08-18 15:27:26 +0300

    first 2 ex

--- src/stores/ShoppingList.js
+++ src/stores/ShoppingList.js
@@ -4,17 +4,22 @@ import { Item } from './Item'
 
 export class ShoppingList {
     // your code here
-    list = [];
-    length;
-    checkItem = () => {
-        // your code here
+    @observable list = [];
+    @observable length;
+    @action checkItem = (name) => {
+        let item = this.list.find(i => i.name === name)
+        item.completed = !item.completed
     }
-    addItem = () => {
-        // your code here
+    @action addItem = (name) => {
+        const newItem = new Item(name)
+        this.list.push(newItem)
     }
-    editItem = () => {
-        // your code here
+    @action editItem = (itemName, newLocation) => {
+        prompt("Change the location")
+        let item = this.list.find(i => i.name === itemName)
+        item.location = newLocation
     }
+
     deleteItem = () => {
         // your code here
     } 
